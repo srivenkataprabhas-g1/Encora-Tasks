114387529323795f18125d0e6fe545fc7f06e5a9
srivenkataprabhas-g1 <srivenkataprabhas.g1@encora.com>
2025-10-08 10:48:58 +0530

    Delete SBH2DB directory

--- SBH2DB/src/main/java/com/prabhas/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prabhas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prabhas.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-
-}
